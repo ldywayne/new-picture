7fc631b3f9c5e2bd7fac0c119cb4e4e2925a9bc5
luodongyang <2010477739@qq.com>
2025-12-05 09:19:03 +0800

    添加 App.vue 和 BasicLayout.vue 组件文件

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from "@/Layouts/BasicLayout.vue";
+import { useUserLoginUserStore } from "@/stores/userLoginUserStore";
+
+const loginUserStore = useUserLoginUserStore();
+// 从后端获取登录用户信息
+loginUserStore.fetchLoginUser()
+</script>
+<style>
+#app {}
+</style>
---
+++ src/Layouts/BasicLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="basic-layout">
+    <a-layout style="min-height: 100vh;">
+      <a-layout-header class="header">
+        <BasicHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.codefather.cn" target="_blank">
+          云图库2.0 by luodongyang
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<script setup lang="ts">
+import BasicHeader from "@/components/BasicHeader.vue";
+
+
+
+</script>
+
+<style scoped>
+#basic-layout .header {
+  /* border: 1px solid red; */
+  background-color: white;
+  padding-inline: 20px;
+  margin-bottom: 12px;
+}
+
+#basic-layout .content {
+  /* border: 1px solid red; */
+  background-color: white;
+  padding: 20px;
+  margin-bottom: 30px;
+}
+
+#basic-layout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
